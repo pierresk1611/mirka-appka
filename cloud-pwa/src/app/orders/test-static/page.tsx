43968519885aead8cf3e4bc19c980f045bb484e0
pierresk1611 <peter.duris.zv@gmail.com>
2026-02-03 15:57:43 +0100

    Debug: Simplify Order route and add nested static test

---
+++ cloud-pwa/src/app/orders/test-static/page.tsx
@@ -0,0 +1,8 @@
+export default function StaticOrderTest() {
+    return (
+        <div className="p-10">
+            <h1 className="text-xl">Static Nested Route Works</h1>
+            <p>/orders/test-static</p>
+        </div>
+    );
+}
