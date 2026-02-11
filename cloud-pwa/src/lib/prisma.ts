9b5996ece9fd7e808c646861bbffe1c3f017a499
pierresk1611 <peter.duris.zv@gmail.com>
2026-02-11 13:42:05 +0100

    Fix: Forced database push and prisma singleton

--- cloud-pwa/src/lib/prisma.ts
+++ cloud-pwa/src/lib/prisma.ts
@@ -1,14 +1,4 @@
-
 import { PrismaClient } from '@prisma/client'
-
-const prismaClientSingleton = () => {
-    return new PrismaClient()
-}
-
-declare global {
-    var prisma: undefined | ReturnType<typeof prismaClientSingleton>
-}
-
-export const prisma = globalThis.prisma ?? prismaClientSingleton()
-
-if (process.env.NODE_ENV !== 'production') globalThis.prisma = prisma
+const globalForPrisma = global as unknown as { prisma: PrismaClient }
+export const prisma = globalForPrisma.prisma || new PrismaClient()
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
