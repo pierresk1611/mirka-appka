3ab2686e5ef899c09b277090388ed7c02c897835
pierresk1611 <peter.duris.zv@gmail.com>
2026-02-11 21:11:16 +0100

    fix(db): revert to PRISMA_DATABASE_URL for Vercel compatibility

--- cloud-pwa/src/lib/prisma.ts
+++ cloud-pwa/src/lib/prisma.ts
@@ -4,7 +4,7 @@ const prismaClientSingleton = () => {
   return new PrismaClient({
     datasources: {
       db: {
-        url: process.env.DATABASE_URL,
+        url: process.env.PRISMA_DATABASE_URL,
       },
     },
   })
